b119d975384b7e68a9b40e53177aa1e6255492bb
JongHoonJang <jangjr98@gmail.com>
2023-07-23 09:58:28 +0900

    webserver

---
+++ server_side_javascript/webserver.js
@@ -0,0 +1,12 @@
+// webserver를 만드는 코드
+const http = require('http');
+ 
+const hostname = '127.0.0.1';
+const port = 1337;
+ 
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
