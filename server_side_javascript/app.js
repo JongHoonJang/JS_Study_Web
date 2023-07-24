3fe8f4bc8b8393610a28e07f5cbe101ea576157e
JongHoonJang <jangjr98@gmail.com>
2023-07-24 21:08:02 +0900

    app.js

---
+++ server_side_javascript/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello home page');
+});
+app.get('/login', (req, res) => {
+  res.send('<h1>Login please</h1>');
+});
+app.listen(3000, () => {
+  console.log('Conneted 3000 port!');
+});
