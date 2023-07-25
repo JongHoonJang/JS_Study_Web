9012b8c1c0987257497a28e5fe055f3181d6ffe7
JongHoonJang <jangjr98@gmail.com>
2023-07-25 20:25:56 +0900

    html

--- server_side_javascript/app.js
+++ server_side_javascript/app.js
@@ -4,6 +4,30 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.send('Hello home page');
 });
+app.get('/dynamic', (req, res) =>{
+  let lis = '';
+  for(let i=0; i<5; i++){
+    lis = lis + '<li>coding</li>';
+  }
+  let time = Date();
+  const output = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+    <body>
+      Hello, Dynamic!
+      <ul>
+      ${lis}
+      </ul>
+      ${time}
+    </body>
+  </html>`
+  res.send(output)
+});
 app.get('/route', (req, res) => {
   res.send('Hello Router, <img src="route.jpg">')
 });
